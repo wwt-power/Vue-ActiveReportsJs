9df9b36e7ee9fe34bdca72fefff2f5cf7ff90f53
286692865@qq.com <286692865@qq.com>
2021-01-27 17:05:16 +0800

    隐藏侧边栏

--- src/components/printPreview.vue
+++ src/components/printPreview.vue
@@ -11,39 +11,64 @@
 
 <script>
 	import printOpenTitle from "./printOpenTitle.vue"
-	import {Viewer} from "@grapecity/activereports-vue";
+	import {
+		Viewer
+	} from "@grapecity/activereports-vue";
 	import "@grapecity/activereports/styles/ar-js-ui.css";
 	import "@grapecity/activereports/styles/ar-js-viewer.css";
-	
+
 	import "@grapecity/activereports/pdfexport";
 	import "@grapecity/activereports/htmlexport";
 	import "@grapecity/activereports/xlsxexport";
 	import "@grapecity/activereports-localization";
-	
+
 	export default {
 		components: {
 			JSViewer: Viewer,
 			printOpenTitle
 		},
-		data(){
-			return{
+		data() {
+			return {
 				// 弹框名称
-				printOpenTitle:"打印预览"
+				printOpenTitle: "打印预览"
 			}
 		},
-		methods:{
+		methods: {
 			// 打印
-			printFun: function(path,params) {
+			printFun: function(path, params) {
 				//加载报表
-				this.$refs.reportViewer.Viewer().open(path , params);
+				this.$refs.reportViewer.Viewer().open(path, params);
 			},
 			// 退出
-			exitPreview:function(){
-				this.$emit('closePreview',false);
+			exitPreview: function() {
+				this.$emit('closePreview', false);
 			}
+		},
+		mounted() {
+			// this.$refs.reportViewer.Viewer().toolbar.updateLayout({
+			//   default: [
+			//     "$zoom",
+			//     "$split",
+			//     "$fullscreen",
+			//     "$split",
+			//     "$print",
+			//     "$split",
+			//     "$about",
+			//   ],
+			//   fullscreen: ["$fullscreen", "$split", "$print", "$split", "$about"],
+			//   mobile: ["$navigation", "$split", "$about"],
+			// });
+			// 侧边栏隐藏
+			this.$refs.reportViewer.Viewer().toggleSidebar();
 		}
 	}
 </script>
 
-<style>
+<style scoped>
+	.printFullscreen >>> .gcv-header-container--fullscreen{
+		position: fixed;
+	}
+	.printFullscreen >>> .gc-viewer{
+		background: #eff3f9;
+	}
 </style>
